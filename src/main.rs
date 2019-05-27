c4e919c4591232d15805a9c7b246d79828aa9f7f
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-26 21:02:43 -0700

    remove example code

--- src/main.rs
+++ src/main.rs
@@ -21,22 +21,6 @@ pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // Initialize the core OS
     arbor_os::init();
 
-    use x86_64::structures::paging::Page;
-    use x86_64::VirtAddr;
-
-    let mut mapper = unsafe { memory::init(boot_info.physical_memory_offset) };
-    let mut frame_allocator = unsafe {
-        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
-
-    // map a previously unmapped page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
-
     // Launch test (when built as a test)
     #[cfg(test)]
     test_main();
